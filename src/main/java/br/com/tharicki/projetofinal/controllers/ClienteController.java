baf6dedbaac7807a2994f54e70743d83be768e28
Tharicki <tharicki@hotmail.com>
2015-11-23 10:28:18 -0200

    Metodo post

--- src/main/java/br/com/tharicki/projetofinal/controllers/ClienteController.java
+++ src/main/java/br/com/tharicki/projetofinal/controllers/ClienteController.java
@@ -3,10 +3,7 @@ package br.com.tharicki.projetofinal.controllers;
 import br.com.tharicki.projetofinal.models.Cliente;
 import br.com.tharicki.projetofinal.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,27 @@ public class ClienteController {
     @RequestMapping("/clientes")
     public List<Cliente> findAllClientes(){
         return clienteService.findAllObjects();
+    }
+
+    @RequestMapping(value = "/clientes",method = RequestMethod.POST)
+    public Cliente save(@RequestBody Cliente cliente){
+        try {
+            clienteService.persist(cliente);
+        } catch (Exception e){
+            e.printStackTrace();
+            clienteService.update(cliente);
+        }
+        return cliente;
+    }
+
+    @RequestMapping(value = "/clientes/{id}",method = RequestMethod.POST)
+    public Cliente update(@PathVariable("id") Integer id, @RequestBody Cliente cliente){
+        try {
+            clienteService.persist(cliente);
+        } catch (Exception e){
+            clienteService.update(cliente);
+        }
+        return cliente;
     }
 
 }
